723e67ff2dc757c0b2ffbad68f77529c14fc5654
Kirti Singh <117595341+kirti1210@users.noreply.github.com>
2023-05-07 13:32:46 +0530

    Create authcontroller.js

---
+++ src/controllers/authcontroller.js
@@ -0,0 +1,26 @@
+let checkLoggedIn = (req, res, next) => {
+    if(!req.isAuthenticated()){
+        console.log(req.isAuthenticated())
+        return res.redirect("/login");
+    }
+    next();
+};
+
+let checkLoggedOut = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return res.redirect("/users");
+    }
+    next();
+};
+
+let postLogOut = (req, res) => {
+    req.session.destroy( function(error) {
+        return res.redirect("/login");
+    });
+};
+
+module.exports = {
+    checkLoggedIn: checkLoggedIn,
+    checkLoggedOut: checkLoggedOut,
+    postLogOut: postLogOut
+};
